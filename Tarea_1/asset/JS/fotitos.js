f79e6485f64d42e0995529aaaa47bf37064d649d
SrMesa1 <118227323+SrMesa1@users.noreply.github.com>
2025-03-13 15:09:37 -0500

    ACTUALIZACION

---
+++ Tarea_1/asset/JS/fotitos.js
@@ -0,0 +1,8 @@
+const carousel = new bootstrap.Carousel('#myCarousel')
+
+/*const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})*/
